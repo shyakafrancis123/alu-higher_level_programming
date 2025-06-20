7965fd5c0c03735b2e94ce63b46329ca9b0a317d
shyakafrancis123 <s.francis@alustudent.com>
2025-06-20 22:11:09 +0200

    100

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 for i in range(25, -1, -1):
     if i % 2 == 0:
-        # even index: lowercase
+        # Even index: lowercase letter
         print("{}".format(chr(i + 97)), end="")
     else:
-        # odd index: uppercase
+        # Odd index: uppercase letter
         print("{}".format(chr(i + 65)), end="")
