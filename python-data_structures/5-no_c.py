68878140b670a74f01f73e8bbd9308c86805c3ac
shyakafrancis123 <s.francis@alustudent.com>
2025-06-20 23:59:59 +0200

    5

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-def new_in_list(my_list, idx, element):
-    # Return a copy of the original list if idx is invalid
-    if idx < 0 or idx >= len(my_list):
-        return my_list[:]
-    # Create a copy of the list
-    new_list = my_list[:]
-    # Replace the element at idx
-    new_list[idx] = element
-    return new_list
+def no_c(my_string):
+    result = ""
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            result += char
+    return result
