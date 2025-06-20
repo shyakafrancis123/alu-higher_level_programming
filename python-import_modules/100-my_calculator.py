8dbdace1e22ea31c481291dc8ee6335ff1175587
shyakafrancis123 <s.francis@alustudent.com>
2025-06-20 23:21:05 +0200

    100*

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -3,19 +3,19 @@ from calculator_1 import add, sub, mul, div
 import sys
 
 
-def usage_and_exit():
+def usage_exit():
     print("Usage: ./100-my_calculator.py <a> <operator> <b>")
     sys.exit(1)
 
 
-def unknown_operator_and_exit():
+def unknown_operator_exit():
     print("Unknown operator. Available operators: +, -, * and /")
     sys.exit(1)
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        usage_and_exit()
+        usage_exit()
 
     a_str, operator, b_str = sys.argv[1], sys.argv[2], sys.argv[3]
 
@@ -23,7 +23,7 @@ if __name__ == "__main__":
         a = int(a_str)
         b = int(b_str)
     except ValueError:
-        usage_and_exit()
+        usage_exit()
 
     if operator == '+':
         result = add(a, b)
@@ -34,6 +34,6 @@ if __name__ == "__main__":
     elif operator == '/':
         result = div(a, b)
     else:
-        unknown_operator_and_exit()
+        unknown_operator_exit()
 
     print(f"{a} {operator} {b} = {result}")
