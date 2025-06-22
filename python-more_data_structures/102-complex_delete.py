f02e1f16c3520d0ed4891d356480d969deeee2e6
shyakafrancis123 <s.francis@alustudent.com>
2025-06-22 10:41:31 +0200

    102

--- python-more_data_structures/102-complex_delete.py
+++ python-more_data_structures/102-complex_delete.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    # Create a copy of the dictionary without keys whose value equals the given value
     return {k: v for k, v in a_dictionary.items() if v != value}
+
