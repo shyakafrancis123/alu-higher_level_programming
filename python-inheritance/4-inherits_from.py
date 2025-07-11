5aefb2a2c44b8558dfc5608f3a43908dc5f9fca2
shyakafrancis123 <s.francis@alustudent.com>
2025-07-11 20:15:53 +0200

    qall

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,1 +1,7 @@
-Yes
+#!/usr/bin/python3
+"""Module that defines a function to check only subclass inheritance."""
+
+
+def inherits_from(obj, a_class):
+    """Return True if obj is an instance of a subclass of a_class."""
+    return isinstance(obj, a_class) and type(obj) is not a_class
