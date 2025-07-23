6e49f3fd42161b1b901d4c46020c1cdd8c9ea715
shyakafrancis123 <s.francis@alustudent.com>
2025-07-23 16:32:14 +0200

    jhjfj

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -22,11 +22,11 @@ def matrix_divided(matrix, div):
         ZeroDivisionError: If div is zero.
     """
     if (not isinstance(matrix, list) or
-            not all(isinstance(row, list) for row in matrix)):
-        raise TypeError("atrix must be a matrix (list of lists) of integers/floats")
+        not all(isinstance(row, list) for row in matrix)):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
 
     if not all(isinstance(x, (int, float)) for row in matrix for x in row):
-        raise TypeError("atrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
 
     row_lengths = list(map(len, matrix))
     if len(set(row_lengths)) != 1:
