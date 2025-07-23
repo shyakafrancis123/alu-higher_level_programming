319fe01caa35444bcc1868bd5d61d96b6226bf91
shyakafrancis123 <s.francis@alustudent.com>
2025-07-23 10:48:22 +0200

    hhf

--- python-network_0/102-catch_me.sh
+++ python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Makes a request to catch_me that causes the server to respond with "You got me!"
-curl -sL -X PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "Origin: School"
+curl -sL -X PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "Origin: HolbertonSchool"
